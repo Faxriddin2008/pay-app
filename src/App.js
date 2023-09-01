3ffb42604277c22aeb4522d6471ccdf1edf1d9b9
Faxriddin2008 <fabduvasiyev@gmail.com>
2023-09-02 00:29:23 +0500

    feat:add custom styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import styles from "./utils/style";
+import { Navbar } from "./components";
 
 const App = () => {
   return (
     <div className="bg-primary p-5 w-full overflow-hidden">
-      <div className="px-16 py-5 text-white">Navbar</div>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.container}`}>
+          <Navbar />
+        </div>
+      </div>
     </div>
   );
 };
