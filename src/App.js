f2f1b0526b025a615bc373fd873d9565135fc3ca
Faxriddin2008 <fabduvasiyev@gmail.com>
2023-09-17 00:03:21 +0500

    feat: statistics completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import styles from "./utils/style";
 import { Home, Navbar } from "./components";
 import Statistics from "./components/statistics";
+import { statistics } from "./utils/constants";
 const App = () => {
+  console.log(statistics);
   return (
     <div className="bg-primary p-5 w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
